4403f26eae557404b98868aeae78f5cc68ae21db
Ali <ma.essa.pk@gmail.com>
2017-03-03 15:15:49 +0000

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="panel panel-info">
+  <div class="panel-heading">
+    <h1>ShareInspire</h1>
+  </div>
+  <div class="panel-body">
+    <p>A forum to share and inspire.</p>
+  </div>
+</div>
+
+
+<div class="panel panel-default">
+  <div class="panel-heading text-center">Topics</div>
+
+  <div class="panel-body text-center">
+    <!-- Table -->
+    <table class="table">
+       <thead>
+         <tr>
+           <th>Title</th>
+           <th>Created By</th>
+           <th>Date Created</th>         
+         </tr>
+       </thead>
+
+       <tbody>
+         
+       </tbody>
+    </table>
+  </div>
+</div>
+
+@endsection
