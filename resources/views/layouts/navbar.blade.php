192f162784a9228b23d71e469374834487a2f924
Ali <ma.essa.pk@gmail.com>
2017-07-05 17:48:18 +0100

    Set the Navigation Bar Logo link url to home page

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,7 +8,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">ShareInspire</a>
+          <a class="navbar-brand" href="{{ route('home') }}">ShareInspire</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
