e3f09ecde9ba230e7171871ecf4604994cee54b1
Ali <ma.essa.pk@gmail.com>
2017-08-20 12:04:59 +0100

    Include the app.js script, instead of including bootstrap and jQuery scripts via CDN

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
     <title> @yield('title') </title>
-    <!-- Latest compiled and minified CSS -->
+    
     <link rel="stylesheet" href="/css/app.css">
 
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
