14553f49ffa6b9b2c08bb336c301803d5d964302
Ali <ma.essa.pk@gmail.com>
2017-08-20 12:39:49 +0100

    Display latest posts and also show how long ago they were posted

--- resources/views/posts/latest.blade.php
+++ resources/views/posts/latest.blade.php
@@ -25,18 +25,21 @@
         <p>
           {{ $post->content }} 
 
-          <strong>Posted by</strong>
-
-          <a href="{{ route('profiles.show', $post->user->id) }}">
-            {{ $post->user->name }}
-          </a>
+          <strong>Posted {{ $post->created_at->diffForHumans() }} by
+            <a href="{{ route('profiles.show', $post->user->id) }}">
+              {{ $post->user->name }}
+            </a>
+          </strong>
 
           under the 
-          <strong>Topic</strong> 
-
-          <a href="{{ route('posts.index', $post->topic->id) }}">
-            {{$post->topic->title}}
-          </a>  
+          
+          <strong>
+            Topic
+            <a href="{{ route('posts.index', $post->topic->id) }}">
+              {{$post->topic->title}}
+            </a>  
+          </strong> 
+          
         </p>
       </li>
 
