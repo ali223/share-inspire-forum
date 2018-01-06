2470aacb00f9e4eb7e97061ebe64dbe8372a62cd
Ali <ma.essa.pk@gmail.com>
2018-01-06 22:07:40 +0000

    Improve the design of admin login page and add the required attribute to the input fields for client side validation

--- resources/views/adminsessions/create.blade.php
+++ resources/views/adminsessions/create.blade.php
@@ -3,46 +3,39 @@
 @section('title', "ShareInspire Forum | ADMIN Login")
 
 @section('content')
+<div class="container margin-top">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3">
+      <div class="panel panel-default">
+        <div class="panel-heading text-center">
+          <h3>ADMIN Login Form</h3>
+        </div>
 
-<div class="panel panel-info">
-  <div class="panel-heading">
-    <h1>ShareInspire</h1>
-  </div>
-  <div class="panel-body">
-    <p>A forum to share and inspire.</p>
-  </div>
-</div>
+        @include('layouts.errors')
 
-<div class="panel panel-default">
-  <div class="panel-heading text-center">
-    <h3><strong>ADMIN Login Form</strong></h3>
-  </div>
+        <div class="panel-body text-left">
+          <form method="POST" action=" {{ route('adminsessions.store') }}" enctype="multipart/form-data">
 
-  <hr>
+            {{ csrf_field() }}
 
-  @include('layouts.errors')
+            <div class="form-group">
+              <label for="email">Your Email Address:</label>
+              <input type="email" id="email" name="email" class="form-control" value="{{ old('email') }}" required>
+            </div>
 
-  <div class="panel-body text-left">
-    <form method="POST" action=" {{ route('adminsessions.store') }}" enctype="multipart/form-data">
+            <div class="form-group">
+              <label for="password">Your Password:</label>
+              <input type="password" id="password" name="password" class="form-control" required>
+            </div>
 
-      {{ csrf_field() }}
+            <div class="form-group">
+              <input type="submit" value="Login" class="btn btn-primary" class="form-control">
+            </div>
 
-      <div class="form-group">
-        <label for="email">Your Email Address:</label>
-        <input type="text" id="email" name="email" class="form-control" value="{{ old('email') }}">
-      </div>      
-
-      <div class="form-group">
-        <label for="password">Your Password:</label>
-        <input type="password" id="password" name="password" class="form-control">
-      </div>      
-
-      <div class="form-group">
-        <input type="submit" value="Login" class="btn btn-primary" class="form-control">
-      </div>      
-
-    </form>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
-
 @endsection
