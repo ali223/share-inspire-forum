961b823470fa90e212545d7fec805eee7f6817d9
Ali <ma.essa.pk@gmail.com>
2018-01-06 21:51:12 +0000

    Remove unused external stylesheet links

--- resources/views/adminlayouts/header.blade.php
+++ resources/views/adminlayouts/header.blade.php
@@ -4,17 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    
     <title> @yield('title') </title>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="/css/app.css" >
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet">
 
-    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @yield('links')
 
