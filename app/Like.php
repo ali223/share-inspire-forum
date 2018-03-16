ba76e51f272c160ff81187ba5be300cc7d922454
Ali <ma.essa.pk@gmail.com>
2018-03-16 16:38:09 +0000

    Create the Like model class and the database migration for the likes table

---
+++ app/Like.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $guarded = [];
+    
+    public function user()
+    {
+        $this->belongsTo(User::class);
+    }
+
+    public function likeable()
+    {
+        return $this->morphTo();
+    }
+}
