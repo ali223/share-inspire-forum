70be98d1d9ba33cf7f8f3963a9fb45da73f7ab17
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-04-24 22:13:40 +0100

    Move UserWelcomeEmailTest to the Registrations folder, rename to StoreTest and use the route helper instead of the url

---
+++ tests/Feature/Registrations/StoreTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature\Registrations;
+
+use App\Mail\WelcomeMail;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
+use Tests\TestCase;
+
+class StoreTest extends TestCase
+{
+    use RefreshDatabase,
+        WithFaker;
+
+    /** @test */
+    public function when_a_new_non_admin_user_registers_they_receive_a_welcome_email()
+    {
+        Mail::fake();
+
+        $userRegistrationData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+            'about' => $this->faker->paragraph
+        ];
+
+        $this->post(route('registrations.store'), $userRegistrationData)
+            ->assertRedirect(route('home'))
+            ->assertSessionHas('message', 'User signed up successfully');
+
+        Mail::assertQueued(WelcomeMail::class, function ($mail) use ($userRegistrationData) {
+            return $mail->hasTo($userRegistrationData['email']);
+        });
+    }
+}
