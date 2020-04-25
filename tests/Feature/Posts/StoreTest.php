85e34d4dbb6aab67a122fae7e0bdf0b1a102c6a9
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-04-25 09:53:25 +0100

    Remove NewPostNotificationTest class and move the tests to Posts\StoreTest class

--- tests/Feature/Posts/StoreTest.php
+++ tests/Feature/Posts/StoreTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature\Posts;
 
 use App\Events\NewPostCreated;
+use App\Notifications\NewPostInYourTopic;
 use App\Post;
 use App\Topic;
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Notification;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -16,6 +18,14 @@ class StoreTest extends TestCase
 {
     use RefreshDatabase,
         WithFaker;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        Event::fake();
+        Notification::fake();
+    }
 
     /** @test */
     public function guests_cannot_add_new_posts()
@@ -36,8 +46,6 @@ class StoreTest extends TestCase
     /** @test */
     public function non_admin_users_can_add_new_posts()
     {
-        Event::fake();
-
         $user = factory(User::class)->create();
         $topic = factory(Topic::class)->create();
 
@@ -57,5 +65,48 @@ class StoreTest extends TestCase
         ]);
 
         Event::assertDispatched(NewPostCreated::class);
+    }
+
+    /** @test */
+    public function the_topic_creator_gets_notified_when_a_new_post_is_added_by_another_user()
+    {
+        $topicCreator = factory(User::class)->create();
+        $anotherUser = factory(User::class)->create();
+
+        $topic = factory(Topic::class)->create([
+            'user_id' => $topicCreator->id
+        ]);
+
+        $postData = [
+            'content' => $this->faker->paragraph,
+            'topic_id' => $topic->id
+        ];
+
+        $this->actingAs($anotherUser)
+            ->postJson(route('posts.store', $topic), $postData)
+            ->assertStatus(Response::HTTP_OK);
+
+        Notification::assertSentTo($topicCreator, NewPostInYourTopic::class);
+    }
+
+    /** @test */
+    public function the_topic_creator_does_not_get_notified_when_they_add_a_new_post_themselves()
+    {
+        $topicCreator = factory(User::class)->create();
+
+        $topic = factory(Topic::class)->create([
+            'user_id' => $topicCreator->id
+        ]);
+
+        $postData = [
+            'content' => $this->faker->paragraph,
+            'topic_id' => $topic->id
+        ];
+
+        $this->actingAs($topicCreator)
+            ->postJson(route('posts.store', $topic), $postData)
+            ->assertStatus(Response::HTTP_OK);
+
+        Notification::assertNotSentTo($topicCreator, NewPostInYourTopic::class);
     }
 }
