aabe507aff63a906ad47db429957f866c1c4527b
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-06-17 14:10:31 +0100

    Update the profile show view for the bootstrap 4

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,79 +4,89 @@
 
 @section('content')
   <div class="row">
-    <div class="col-md-10 col-md-offset-1">
-      <div class="page-header">
-        @if ($profileImageUrl)
-          <img class="profile-image" width="125" src="{{ $profileImageUrl }}">
-        @else
-          <span class="glyphicon glyphicon-user profile-icon"></span>
-        @endif
+    <div class="col-md-10 offset-md-1">
+      <div class="card mb-3">
+        <div class="card-body">
+          @if ($profileImageUrl)
+            <img class="profile-image" width="125" src="{{ $profileImageUrl }}">
+          @else
+            <i class="fa fa-5x fa-user text-secondary"></i>
+          @endif
 
-        <h2>
-          {{ $user->name }}
-          <small>Member since {{ $user->created_at->diffForHumans() }}</small>
-        </h2>
+          <h3>
+            {{ $user->name }}
+            <small>
+              Member since {{ $user->created_at->diffForHumans() }}
+            </small>
+          </h3>
 
-        @can('update', $user)
-          <a href="{{ route('profiles.edit', $user) }}"
-            class="btn btn-sm btn-primary">
-            Edit Profile    
-          </a>
-        @endcan
+          @can('update', $user)
+            <a href="{{ route('profiles.edit', $user) }}"
+              class="btn btn-sm btn-custom">
+              Edit Profile
+            </a>
+          @endcan
+        </div>
       </div>
 
-      <div class="panel panel-info">
-        <div class="panel-heading">
-          <h4>
+      <div class="card mb-3">
+        <div class="card-header bg-secondary text-light">
+          <h5>
             About {{ $user->name }}
-          </h4>
+          </h5>
         </div>
-        <div class="panel-body">
+        <div class="card-body">
           <p>
             {{ $user->about }}
           </p>
         </div>
       </div>
 
-      <div class="panel panel-info">
-        <div class="panel-heading">
-          <h4>Topics created by {{ $user->name }}</h4>
+      <div class="card mb-3">
+        <div class="card-header bg-secondary text-light">
+          <h5>
+            Topics created by {{ $user->name }}
+          </h5>
         </div>
-        <div class="panel-body">
+        <div class="card-body">
           <ul>
-              @forelse($user->topics as $topic)
-                <li>
-                    <a href="{{ route('posts.index', $topic) }}">
-                      {{ $topic->title }} 
-                      @if (!$topic->approved)
-                        <em>
-                          (waiting for approval by website admin)
-                        </em>
-                      @endif
-                    </a>
+            @forelse($user->topics as $topic)
+              <li>
+                <a href="{{ route('posts.index', $topic) }}" class="text-custom">
+                  {{ $topic->title }} 
+                  @if (!$topic->approved)
                     <em>
-                      Created on {{ $topic->created_at->toDayDateTimeString() }}
-                    </em> 
-                </li>
-              @empty
-                <li>No Topics Created yet.</li>
-              @endforelse
+                      (waiting for approval by website admin)
+                    </em>
+                  @endif
+                </a>
+                <em>
+                  Created on {{ $topic->created_at->toDayDateTimeString() }}
+                </em> 
+              </li>
+            @empty
+              <li>No Topics Created yet.</li>
+            @endforelse
           </ul>
         </div>
       </div>
 
-      <div class="panel panel-info">
-        <div class="panel-heading">
-          <h4>Posts created by {{ $user->name }}</h4>
+      <div class="card mb-3">
+        <div class="card-header bg-secondary text-light">
+          <h5>
+            Posts created by {{ $user->name }}
+          </h5>
         </div>
-        <div class="panel-body">
+        <div class="card-body">
           <ul>
             @forelse($user->posts as $post)
               <li>
-                <a href="{{ route('posts.index', $post->topic) }}">
+                <a href="{{ route('posts.index', $post->topic) }}" class="text-custom">
                   {{ substr($post->content, 0 ,80) }}....
                 </a>
-                <em>Created on {{ $post->created_at->toDayDateTimeString() }}</em> 
+                <em>
+                  Created on {{ $post->created_at->toDayDateTimeString() }}
+                </em> 
               </li>
             @empty
               <li>No Posts Created yet.</li>
