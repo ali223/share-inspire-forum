18fc0db872f055ecba5bb00cef43fd8f4f3b2c60
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-06-18 14:20:45 +0100

    Update the admin login view for the bootstrap 4

--- resources/views/admin/sessions/create.blade.php
+++ resources/views/admin/sessions/create.blade.php
@@ -4,13 +4,15 @@
 
 @section('content')
   <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-      <div class="panel panel-default">
-        <div class="panel-heading text-center">
-          <h3>ADMIN Login Form</h3>
+    <div class="col-md-6 offset-md-3">
+      <div class="card mt-2 mb-4">
+        <div class="card-header text-center bg-secondary text-light">
+          <h4>
+            ADMIN Login Form
+          </h4>
         </div>
 
-        <div class="panel-body text-left">
+        <div class="card-body">
           <form method="POST" action="{{ route('admin.sessions.store') }}">
             {{ csrf_field() }}
 
@@ -25,7 +27,7 @@
             </div>
 
             <div class="form-group">
-              <input type="submit" value="Login" class="btn btn-primary" class="form-control">
+              <input type="submit" value="Login" class="btn btn-custom">
             </div>
           </form>
         </div>
