abe25f359eb8bff1312c28d8a1222ca264ec53f8
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-06-18 22:23:25 +0100

    Update the admin dashboard view for the bootstrap 4

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,14 +4,14 @@
 
 @section('content')
 
-<header id="header">
+<header id="header" class="py-3">
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1>
-          <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+        <h2>
+          <i class="fa fa-lg fa-dashboard"></i>
           Admin Dashboard
-        </h1>
+        </h2>
       </div>
     </div>
   </div>
@@ -23,58 +23,70 @@
       <div class="col-md-12">
         @include('adminlayouts.message')
 
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <span class="glyphicon glyphicon-stats" aria-hidden="true"></span>
+        <div class="card mt-2 mb-4">
+          <div class="card-header text-left bg-secondary text-light">
+            <i class="fa fa-lg fa-feed"></i>
             Website Overview
           </div>
-          <div class="panel-body">
+          <div class="card-body">
             <div class="row">
               <div class="col-md-3">
-                <div class="well dash-box">
-                  <h3>
-                    <span class="glyphicon glyphicon-list" aria-hidden="true"></span>
-                    {{ $categoriesCount }} Categories
-                  </h3>
-                  <p>
-                    <a href="{{ route('admin.categories.index') }}">
-                      Create / Edit / View Categories
-                    </a>
-                  </p>
+                <div class="card shadow bg-light">
+                  <div class="card-body text-center py-4">
+                    <h4 class="mt-2">
+                      <i class="fa fa-lg fa-list"></i>
+                      {{ $categoriesCount }} Categories
+                    </h4>
+                    <p>
+                      <a href="{{ route('admin.categories.index') }}" class="text-custom">
+                        Create / Edit / View
+                      </a>
+                    </p>
+                  </div>
                 </div>
               </div>
 
               <div class="col-md-3">
-                <div class="well dash-box">
-                  <h3>
-                    <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
-                    {{ $topicsCount }} Topics 
-                  </h3>
-                  <p>
-                    <a href="{{ route('admin.topics.index') }}">
-                      {{ $unapprovedTopicsCount }} topics, pending approval
-                    </a>
-                  </p>
+                <div class="card shadow bg-light">
+                  <div class="card-body text-center py-4">
+                    <h4 class="mt-2">
+                      <i class="fa fa-lg fa-book"></i>
+                      {{ $topicsCount }} Topics 
+                    </h4>
+                    <p>
+                      <a href="{{ route('admin.topics.index') }}" class="text-custom">
+                        {{ $unapprovedTopicsCount }} topics, pending approval
+                      </a>
+                    </p>
+                  </div>
                 </div>
               </div>
 
               <div class="col-md-3">
-                <div class="well dash-box">
-                  <h3>
-                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    {{ $postsCount }} Posts
-                  </h3>
-                  <p> {{ $unapprovedPostsCount }} posts, pending approval</p>
+                <div class="card shadow bg-light">
+                  <div class="card-body text-center py-4">
+                    <h4 class="mt-2">
+                      <i class="fa fa-lg fa-pencil"></i>
+                      {{ $postsCount }} Posts
+                    </h4>
+                    <p>
+                      {{ $unapprovedPostsCount }} posts, pending approval
+                    </p>
+                  </div>
                 </div>
               </div>
 
               <div class="col-md-3">
-                <div class="well dash-box">
-                  <h3>
-                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                    {{ $usersCount }} Users 
-                  </h3>
-                  <p>Total users registered</p>
+                <div class="card shadow bg-light">
+                  <div class="card-body text-center py-4">
+                    <h4 class="mt-2">
+                      <i class="fa fa-lg fa-user"></i>
+                      {{ $usersCount }} Users 
+                    </h4>
+                    <p>
+                      Total users registered
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>            
@@ -89,17 +101,14 @@
   <div class="container margin-top">
     <div class="row">
       <div class="col-md-6">
-        <div class="panel panel-default">
-          <div class="panel-heading">            
-            <span class="glyphicon glyphicon-book" aria-hidden="true"></span>
+        <div class="card mt-2 mb-4">
+          <div class="card-header text-left bg-secondary text-light">
+            <i class="fa fa-lg fa-book"></i>
             Newly Created Topics (Last 5)
 
-            <span class="pull-right">
-              <a class="view-all-link" href="{{ route('admin.topics.index') }}">
-                View All
-              </a>
-            </span>
-            
+            <a class="view-all-link float-right" href="{{ route('admin.topics.index') }}">
+              View All
+            </a>
           </div>
           <table class="table table-striped">
             <tr>
@@ -115,21 +124,18 @@
               </tr>
             @endforeach
           </table>
-        </div>        
+        </div>
 
       </div>
       <div class="col-md-6">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+        <div class="card mt-2 mb-4">
+          <div class="card-header text-left bg-secondary text-light">
+            <i class="fa fa-lg fa-user"></i>
             Newly Registered Users (Last 5)
 
-            <span class="pull-right">
-              <a class="view-all-link" href="#">
-                View All
-              </a>
-            </span>
-
+            <a class="view-all-link float-right" href="#">
+              View All
+            </a>
           </div>
           <table class="table table-striped">
             <tr>
