db4d019b3ef920f155eaa48dbebe7922cdfd5efa
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-06-19 18:37:06 +0100

    Convert indentation to 2 spaces for the admin categories related vue components

--- resources/assets/js/components/CategoriesList.vue
+++ resources/assets/js/components/CategoriesList.vue
@@ -1,126 +1,125 @@
 <template>
-<div class="container margin-top" v-if="signedIn">
-  <div class="row">
-    <div class="col-md-8">
-			<div class="panel panel-default">
-				<div class="panel-heading text-center">
-				  <h3>Categories</h3>
-				</div>
+  <div class="container margin-top" v-if="signedIn">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="panel panel-default">
+          <div class="panel-heading text-center">
+            <h3>Categories</h3>
+          </div>
 
-				<div class="panel-body">
-					<table class="table table-striped table-hover">
-						<thead>
-							<tr>
-								<th>Category Id</th>
-								<th>Category Name</th>
-								<th>Category Description</th>
-								<th>Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr :class="{'success' : isSelected(category) }"
-								v-for="category in categories">
-								<td>{{ category.id }}</td>
-								<td>{{ category.name }}</td>
-								<td>{{ category.description }}</td>
-								<td>
-									<button class="btn btn-primary" @click="showEditForm(category)">
-										Edit
-									</button>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-4">
-			<new-category @categoryAdded="addToCategoriesList"
-						v-if="!editing">
-			</new-category>
-			<edit-category v-else
-						:category-data="selectedCategory"
-						@categoryUpdated="updateCategoriesList"
-						@cancelled="cancelEditForm">
-			</edit-category>
-		</div>
-	</div>
-</div>
+          <div class="panel-body">
+            <table class="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>Category Id</th>
+                  <th>Category Name</th>
+                  <th>Category Description</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr :class="{'success' : isSelected(category) }"
+                  v-for="category in categories">
+                  <td>{{ category.id }}</td>
+                  <td>{{ category.name }}</td>
+                  <td>{{ category.description }}</td>
+                  <td>
+                    <button class="btn btn-primary" @click="showEditForm(category)">
+                      Edit
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <new-category 
+          @categoryAdded="addToCategoriesList"
+          v-if="!editing"
+        >
+        </new-category>
+        <edit-category 
+          v-else
+          :category-data="selectedCategory"
+          @categoryUpdated="updateCategoriesList"
+          @cancelled="cancelEditForm"
+        >
+        </edit-category>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 import NewCategory from './NewCategory.vue';
 import EditCategory from './EditCategory.vue';
 
 export default {
-	components: { NewCategory, EditCategory },
+  components: { NewCategory, EditCategory },
 
-	data() {
-		return {
-			categories: [],
-			editing: false,
-			selectedCategory: null
-		}
-	},
+  data() {
+    return {
+      categories: [],
+      editing: false,
+      selectedCategory: null
+    }
+  },
 
-	created() {
-		this.getCategories();
-	},
+  created() {
+    this.getCategories();
+  },
 
-	computed: {
-		signedIn() {
-			return window.App.signedIn;
-		}
-	},
+  computed: {
+    signedIn() {
+      return window.App.signedIn;
+    }
+  },
 
-	methods: {
-		getCategories() {
-			axios.get('/admin/categories')
-					.then(response => this.categories = response.data)
-					.catch(error => {
-							flashMessage('Error retrieving categories', 'danger');
-					});
-		},
+  methods: {
+    getCategories() {
+      axios.get('/admin/categories')
+          .then(response => this.categories = response.data)
+          .catch(error => {
+              flashMessage('Error retrieving categories', 'danger');
+          });
+    },
 
-		addToCategoriesList(newCategoryData) {
-			this.categories.push(newCategoryData);
-			flashMessage('New category added successfully', 'success');
-		},
+    addToCategoriesList(newCategoryData) {
+      this.categories.push(newCategoryData);
+      flashMessage('New category added successfully', 'success');
+    },
 
-		updateCategoriesList(updatedCategoryData) {
-			this.editing = false;
-			this.selectedCategory = null;
+    updateCategoriesList(updatedCategoryData) {
+      this.editing = false;
+      this.selectedCategory = null;
 
-			for (let category of this.categories) {
-				if (category.id == updatedCategoryData.id) {
-					category.name = updatedCategoryData.name;
-					category.description = updatedCategoryData.description;
-				}
-			}
-		},
+      for (let category of this.categories) {
+        if (category.id == updatedCategoryData.id) {
+          category.name = updatedCategoryData.name;
+          category.description = updatedCategoryData.description;
+        }
+      }
+    },
 
-		showEditForm(category) {
-			this.editing = true;
-			this.selectedCategory = category;
-		},
+    showEditForm(category) {
+      this.editing = true;
+      this.selectedCategory = category;
+    },
 
-		cancelEditForm() {
-			this.editing = false;
-			this.selectedCategory = null;
-		},
+    cancelEditForm() {
+      this.editing = false;
+      this.selectedCategory = null;
+    },
 
-		isSelected(category) {
-			if (this.selectedCategory) {
-				return category.id == this.selectedCategory.id;
-			}
+    isSelected(category) {
+      if (this.selectedCategory) {
+        return category.id == this.selectedCategory.id;
+      }
 
-			return false;
-		}
-
-
-	}
+      return false;
+    }
+  }
 }
 </script>
-
-
--- resources/assets/js/components/NewCategory.vue
+++ resources/assets/js/components/NewCategory.vue
@@ -1,66 +1,83 @@
 <template>
-    <div class="panel panel-default text-left" v-if="signedIn">
-      <div class="panel panel-heading">
-        <strong>Create a New Category</strong>
-      </div>
-      <div class="panel-body">
-        <form>
-          <div class="form-group">
-            <label for="name">Name</label>
-            <input id="name" name="name" class="form-control" v-model="name" required>
-          </div>
-
-          <div class="form-group">
-            <label for="name">Description</label>
-            <textarea id="description" name="description" class="form-control" v-model="description" required></textarea>
-          </div>
-          <button class="btn btn-primary" type="submit" @click.prevent="addCategory">
-            Save
-          </button>
-        </form>
-      </div>      
+  <div class="panel panel-default text-left" v-if="signedIn">
+    <div class="panel panel-heading">
+      <strong>Create a New Category</strong>
     </div>
+    <div class="panel-body">
+      <form>
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input 
+            id="name" 
+            name="name" 
+            class="form-control" 
+            v-model="name" 
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="name">Description</label>
+          <textarea 
+            id="description" 
+            name="description" 
+            class="form-control" 
+            v-model="description" 
+            required></textarea>
+        </div>
+        <button 
+          class="btn btn-primary" 
+          type="submit" 
+          @click.prevent="addCategory"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-      data() {
-        return {
-          name: null,
-          description: null
-        }
-      },
-      computed: {
-        signedIn() {
-          return window.App.signedIn;
-        }
-      },
-      methods: {
-        addCategory() {
-          if (!this.name) {
-            flashMessage('Error! Please enter the category name.', 'danger');
-            return;
-          }
-
-          if (!this.description) {
-            flashMessage('Error! Please enter the category description', 'danger');
-            return;
-          }
-
-          axios.post(location.pathname, 
-              {
-                name: this.name,
-                description: this.description
-              }
-            )
-          .then(response => {
-              this.name = null;
-              this.description = null;
-              this.$emit('categoryAdded', response.data);
-          }).catch(error => {
-              flashMessage('Error Adding New Category', 'warning');
-          });
-        }
-      }
+export default {
+  data() {
+    return {
+      name: null,
+      description: null
     }
+  },
+
+  computed: {
+    signedIn() {
+      return window.App.signedIn;
+    }
+  },
+
+  methods: {
+    addCategory() {
+      if (!this.name) {
+        flashMessage('Error! Please enter the category name.', 'danger');
+        return;
+      }
+
+      if (!this.description) {
+        flashMessage('Error! Please enter the category description', 'danger');
+        return;
+      }
+
+      axios.post(
+        location.pathname, 
+        {
+          name: this.name,
+          description: this.description
+        }
+      ).then(response => {
+        this.name = null;
+        this.description = null;
+        this.$emit('categoryAdded', response.data);
+      }).catch(error => {
+        flashMessage('Error Adding New Category', 'warning');
+      });
+    }
+  }
+}
 </script>
