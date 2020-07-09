92bc4ff6fb20c1775a44729c62432578b1fed8df
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-07-09 17:02:49 +0100

    Add the 'View Topics' link to the category cards

--- resources/views/categories/_categories-list.blade.php
+++ resources/views/categories/_categories-list.blade.php
@@ -3,9 +3,7 @@
   <div class="row">
     @foreach ($chunk as $category)
       <div class="col-md-4">
-        <a href="{{ route('topics.index', $category->id) }}" class="category-link">
-          @include('categories._category-card')
-        </a>
+        @include('categories._category-card')
       </div>
     @endforeach
   </div>
--- resources/views/categories/_category-card.blade.php
+++ resources/views/categories/_category-card.blade.php
@@ -2,7 +2,10 @@
   <div class="card-header bg-custom-secondary">
     <h4>{{ $category->name }}</h4>
   </div>
-  <div class="card-body">
-    {{ $category->description }}
+  <div class="card-body d-flex flex-column justify-content-between">
+    <p>{{ $category->description }}</p>
+    <a href="{{ route('topics.index', $category) }}" class="btn btn-block btn-custom">
+      View Topics
+    </a>
   </div>
 </div>
