d28003a7014df3e9c2133fe235e38e22e3b98795
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-07-09 18:11:56 +0100

    Add the open graph meta tags to get the website preview on social media

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,6 +4,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:type" content= "website">
+    <meta property="og:url" content="{{ config('app.url') }}">
+    <meta property="og:site_name" content="Share Inspire Forum">
+    <meta property="og:description" content="A discussion forum where we share our experiences and inspire each other.">
+    <meta property="og:image" content="{{ asset('images/website-preview.png') }}" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
